e3433bc21bb720c8cca2a01e2562a0f0f97f8275
subbaSahil <sahil_s@pursuitsoftware.com>
2025-02-13 16:03:25 +0530

    new push

--- src/main/java/MagicWorkShopJenkins/MagicWorkShopJenkins/BaseTest.java
+++ src/main/java/MagicWorkShopJenkins/MagicWorkShopJenkins/BaseTest.java
@@ -4,9 +4,8 @@ import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
+
 import org.testng.annotations.BeforeMethod;
 
 import java.io.FileInputStream;
@@ -20,7 +19,7 @@ public class BaseTest {
 
     public Properties properties = new Properties();
 
-    @BeforeClass
+    @BeforeMethod
     public void setup() throws InterruptedException {
         loadProperties(); // Load properties before setting up the driver
         driver = new EdgeDriver();
@@ -40,7 +39,7 @@ public class BaseTest {
         }
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown() throws InterruptedException {
         if (driver != null) {
             Thread.sleep(2000);
